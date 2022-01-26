a52f8adfcab0326f1857d4aa8a103f04495faee6
Yujin Jeong <jinietree20@gmail.com>
2022-01-26 15:24:10 +0900

    UI done

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -9,4 +9,37 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     
+    var beerList = [Beer]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UINavigationBar
+        title = "브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //UITableView 설정
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
+        tableView.rowHeight = 150
+    }
+}
+
+// UITableView DataSource, Delegate
+extension BeerListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
+        cell.configure(with: self.beerList[indexPath.row])
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBeer = self.beerList[indexPath.row]
+        let detailViewController = BeerDetailViewController()
+        detailViewController.beer = selectedBeer
+        self.show(detailViewController, sender: nil)
+    }
 }
